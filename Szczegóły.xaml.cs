4fd6f84a7469ec4f15c3abf0db4dc4365c2b941e
DonTymono <95697162+DonTymono@users.noreply.github.com>
2023-06-29 22:35:37 +0200

    bindingXML

---
+++ Szczegóły.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Xml.Linq;
+
+namespace bindingXML
+{
+    public partial class Szczegóły : Window
+    {
+        public Szczegóły(XElement kategoria)
+        {
+            InitializeComponent();
+
+            // Ustawienie DataContext na kategorię
+            DataContext = kategoria;
+
+            // Ustawienie ItemsSource dla DataGrid
+            var produkty = kategoria.Element("Produkty").Elements("Produkt");
+            dataGrid.ItemsSource = produkty;
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // Możesz dodać tutaj kod obsługi zdarzenia SelectionChanged, jeśli jest potrzebny
+        }
+    }
+}
